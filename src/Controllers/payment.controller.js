7d6ca226de1e58c57d3209d4f3f4e4df79b9d04c
DeepanshuDahiya <deepanshu31232@gmail.com>
2026-07-18 12:58:54 +0530

    add central error and response handling in payment controllers and use IORedis instead of official redis in payment controllers

--- src/Controllers/payment.controller.js
+++ src/Controllers/payment.controller.js
@@ -4,74 +4,71 @@ import Transactions from "../Models/transaction.model.js";
 import Users from "../Models/user.model.js";
 import Wallets from "../Models/wallet.model.js";
 import { sanitizeUser } from "../Utilities/sanitizeUser.js";
-import { redisClient } from "../../server.js";
-import { performance } from "node:perf_hooks";
+import customError from "../Utilities/customError.js";
+import { redis } from "../Config/redis.js";
+import sendResponse from "../Utilities/sendResponse.js";
 
-export const getReceiver = async (req, res) => {
-  const email = req.params.email;
-
-  if (!email) {
-    res.status(400).json({ error: "Email is required to find receiver." });
-  }
-
-  const receiverEmail = decodeURIComponent(email);
-
+export const getReceiver = async (req, res, next) => {
   try {
-    const user = await Users.findOne({ email: receiverEmail }).lean();
-    if (!user) {
-      return res
-        .status(404)
-        .json({ error: "Enter a valid email of existing user." });
-    }
-    if (user._id.equals(req.user.userId)) {
-      return res
-        .status(400)
-        .json({ error: "Users cannot send money to themselves" });
-    }
-    const safeUser = sanitizeUser(user);
-    return res.json({ safeUser });
+    const { email } = req.params;
+
+    if (!email)
+      throw new customError(400, "Email is required to find receiver");
+
+    const user = await Users.findOne({ email }).lean();
+    if (!user) throw new customError(404, "User not found");
+
+    if (user._id.equals(req.user.userId))
+      throw new customError(400, "Users cannot send money to themselves");
+
+    const sanitizedUser = sanitizeUser(user);
+    return sendResponse(res, 200, "Current User", { receiver: sanitizedUser });
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    next(error);
   }
 };
 
-export const createTransfer = async (req, res) => {
-  const receiverId = req.body.receiverId;
-  const amount = Number(req.body.amount);
-  const idemKey = req.body.idempotencyKey;
-
-  if (!receiverId || typeof amount !== "number" || amount <= 0 || !idemKey) {
-    return res.status(400).json({ error: "Invalid amount or receiverId" });
-  }
-
-  const idempotencyKey = `idem:${req.user.userId}:${idemKey}`;
-
-  const isSet = await redisClient.set(idempotencyKey, "PENDING", {
-    NX: true,
-    EX: 60 * 5,
-  });
-
-  if (!isSet) {
-    const existing = await redisClient.get(idempotencyKey);
-    return res.status(200).json({ status: existing });
-  }
-
+export const createTransfer = async (req, res, next) => {
   const session = await mongoose.startSession();
   let transaction;
-
+  let idempotencyKey;
   try {
-    const receiver = await Users.findById(receiverId);
-    if (!receiver || !receiver.walletId) {
-      return res.status(404).json({ error: "Receiver or wallet not found." });
-    }
-    if (receiver._id.equals(req.user.userId)) {
-      return res
-        .status(400)
-        .json({ error: "Receiver cannot send money to themselves." });
+    const receiverId = req.body.receiverId;
+    const amount = Number(req.body.amount);
+    const idemKey = req.body.idempotencyKey;
+
+    if (!receiverId || typeof amount !== "number" || amount <= 0)
+      throw new customError(400, "Invalid amount or receiver Id");
+
+    if (!idemKey) throw new customError(400, "Idempotency key is required");
+
+    idempotencyKey = `idem:${req.user.userId}:${idemKey}`;
+
+    const isSet = await redis.set(
+      idempotencyKey,
+      "PENDING",
+      "EX",
+      60 * 5,
+      "NX",
+    );
+
+    if (!isSet) {
+      const existing = await redis.get(idempotencyKey);
+      return sendResponse(
+        res,
+        200,
+        "Transaction with this idempotency key exists",
+        { status: existing },
+      );
     }
 
-    const senderObjectId = req.user.userId;
-    const receiverObjectId = receiver._id;
+    const receiver = await Users.findById(receiverId);
+
+    if (!receiver || !receiver.walletId)
+      throw new customError(404, "Receiver or wallet not found");
+
+    if (receiver._id.equals(req.user.userId))
+      throw new customError(404, "Receiver cannot send money to themselves.");
 
     await session.startTransaction();
 
@@ -81,9 +78,7 @@ export const createTransfer = async (req, res) => {
       { session },
     );
 
-    if (!sendResult) {
-      throw new Error("Insufficient balance");
-    }
+    if (!sendResult) throw new customError(400, "Insufficient balance");
 
     const receiveResult = await Wallets.findOneAndUpdate(
       { _id: receiver.walletId },
@@ -91,9 +86,8 @@ export const createTransfer = async (req, res) => {
       { session },
     );
 
-    if (!receiveResult) {
-      throw new Error(`Failed to send money to ${receiver.name}`);
-    }
+    if (!receiveResult)
+      throw new customError(400, `Failed to send money to ${receiver.name}`);
 
     [transaction] = await Transactions.create(
       [
@@ -101,26 +95,26 @@ export const createTransfer = async (req, res) => {
           type: "transfer",
           amount,
           status: "success",
-          senderId: senderObjectId,
-          receiverId: receiverObjectId,
+          senderId: req.user.userId,
+          receiverId: receiver._id,
         },
       ],
       { session },
     );
 
-    if (!transaction) throw new Error("Transaction not updated.");
+    if (!transaction) throw new customError(400, "Transaction not created.");
 
     const ledgerResult = await Ledgers.insertMany(
       [
         {
           type: "debit",
-          userId: senderObjectId,
+          userId: req.user.userId,
           amount: -amount,
           transactionId: transaction._id,
         },
         {
           type: "credit",
-          userId: receiverObjectId,
+          userId: receiver._id,
           amount: amount,
           transactionId: transaction._id,
         },
@@ -130,50 +124,60 @@ export const createTransfer = async (req, res) => {
 
     await session.commitTransaction();
 
-    await redisClient.set(idempotencyKey, "SUCCESS", { EX: 60 * 5 });
+    await redis.set(idempotencyKey, "SUCCESS", "EX", 60 * 5);
 
-    return res.status(200).json({ message: "Transfer Successful" });
+    return sendResponse(res, 200, "Transfer successful");
   } catch (error) {
     if (transaction) {
       await Transactions.findByIdAndUpdate(transaction._id, {
         status: "failed",
       });
-      await redisClient.set(idempotencyKey, "FAILED", { EX: 60 * 5 });
     }
-    await session.abortTransaction();
-
-    return res.status(500).json({ error: error.message });
+    if (idempotencyKey) {
+      await redis.set(idempotencyKey, "FAILED", "EX", 60 * 5);
+    }
+    if (session.inTransaction()) {
+      await session.abortTransaction();
+    }
+    next(error);
   } finally {
     await session.endSession();
   }
 };
 
-export const createDeposit = async (req, res) => {
-  const amount = Number(req.body.amount);
-  const idemKey = req.body.idempotencyKey;
-
-  if (isNaN(amount) || amount <= 0 || !idemKey) {
-    return res
-      .status(400)
-      .json({ message: "Valid amount is required for deposit." });
-  }
-
-  const idempotencyKey = `idem:${req.user.userId}:${idemKey}`;
-
-  const isSet = await redisClient.set(idempotencyKey, "PENDING", {
-    NX: true,
-    EX: 60 * 5,
-  });
-
-  if (!isSet) {
-    const existing = await redisClient.get(idempotencyKey);
-    return res.status(200).json({ status: existing });
-  }
-
+export const createDeposit = async (req, res, next) => {
   const session = await mongoose.startSession();
   let transaction;
-
+  let idempotencyKey;
   try {
+    const amount = Number(req.body.amount);
+    const idemKey = req.body.idempotencyKey;
+
+    if (typeof amount !== "number" || amount <= 0)
+      throw new customError(400, "Invalid amount or receiver Id");
+
+    if (!idemKey) throw new customError(400, "Idempotency key is required");
+
+    idempotencyKey = `idem:${req.user.userId}:${idemKey}`;
+
+    const isSet = await redis.set(
+      idempotencyKey,
+      "PENDING",
+      "EX",
+      60 * 5,
+      "NX",
+    );
+
+    if (!isSet) {
+      const existing = await redis.get(idempotencyKey);
+      return sendResponse(
+        res,
+        200,
+        "Transaction with this idempotency key exists",
+        { status: existing },
+      );
+    }
+
     await session.startTransaction();
 
     const walletResult = await Wallets.findByIdAndUpdate(
@@ -184,7 +188,7 @@ export const createDeposit = async (req, res) => {
       { session },
     );
 
-    if (!walletResult) throw new Error("Wallet not updated.");
+    if (!walletResult) throw new customError(400, "Wallet not updated.");
 
     [transaction] = await Transactions.create(
       [
@@ -198,7 +202,7 @@ export const createDeposit = async (req, res) => {
       { session },
     );
 
-    if (!transaction) throw new Error("Transaction not updated.");
+    if (!transaction) throw new customError(400, "Transaction not updated.");
 
     await Ledgers.create(
       [
@@ -212,52 +216,62 @@ export const createDeposit = async (req, res) => {
       { session },
     );
 
+    await redis.set(idempotencyKey, "SUCCESS", "EX", 60 * 5);
+
     await session.commitTransaction();
 
-    await redisClient.set(idempotencyKey, "SUCCESS", { EX: 60 * 5 });
-
-    return res.status(200).json({ message: "Deposit Successful." });
+    return sendResponse(res, 200, "Deposit successful");
   } catch (error) {
-    await session.abortTransaction();
+    if (session.inTransaction()) {
+      await session.abortTransaction();
+    }
     if (transaction) {
       await Transactions.findByIdAndUpdate(transaction._id, {
         status: "failed",
       });
-      await redisClient.set(idempotencyKey, "FAILED", { EX: 60 * 5 });
     }
-    return res.status(500).json({ error: error.message });
+    if (idempotencyKey) {
+      await redis.set(idempotencyKey, "FAILED", "EX", 60 * 5);
+    }
+    next(error);
   } finally {
     await session.endSession();
   }
 };
 
-export const createWithdrawal = async (req, res) => {
-  const amount = Number(req.body.amount);
-
-  const idemKey = req.body.idempotencyKey;
-
-  if (isNaN(amount) || amount <= 0 || !idemKey) {
-    return res
-      .status(400)
-      .json({ message: "Valid amount is required for withdrawal." });
-  }
-
-  const idempotencyKey = `idem:${req.user.userId}:${idemKey}`;
-
-  const isSet = await redisClient.set(idempotencyKey, "PENDING", {
-    NX: true,
-    EX: 60 * 5,
-  });
-
-  if (!isSet) {
-    const existing = await redisClient.get(idempotencyKey);
-    return res.status(200).json({ status: existing });
-  }
-
+export const createWithdrawal = async (req, res, next) => {
   const session = await mongoose.startSession();
   let transaction;
-
+  let idempotencyKey;
   try {
+    const amount = Number(req.body.amount);
+    const idemKey = req.body.idempotencyKey;
+
+    if (typeof amount !== "number" || amount <= 0)
+      throw new customError(400, "Invalid amount or receiver Id");
+
+    if (!idemKey) throw new customError(400, "Idempotency key is required");
+
+    idempotencyKey = `idem:${req.user.userId}:${idemKey}`;
+
+    const isSet = await redis.set(
+      idempotencyKey,
+      "PENDING",
+      "EX",
+      60 * 5,
+      "NX",
+    );
+
+    if (!isSet) {
+      const existing = await redis.get(idempotencyKey);
+      return sendResponse(
+        res,
+        200,
+        "Transaction with this idempotency key exists",
+        { status: existing },
+      );
+    }
+
     await session.startTransaction();
 
     const walletResult = await Wallets.findOneAndUpdate(
@@ -268,7 +282,7 @@ export const createWithdrawal = async (req, res) => {
       { session },
     );
 
-    if (!walletResult) throw new Error("Wallet not updated.");
+    if (!walletResult) throw new customError(400, "Wallet not updated.");
 
     [transaction] = await Transactions.create(
       [
@@ -282,7 +296,7 @@ export const createWithdrawal = async (req, res) => {
       { session },
     );
 
-    if (!transaction) throw new Error("Transaction not updated.");
+    if (!transaction) throw new customError(400, "Transaction not updated.");
 
     await Ledgers.create(
       [
@@ -298,19 +312,23 @@ export const createWithdrawal = async (req, res) => {
 
     await session.commitTransaction();
 
-    await redisClient.set(idempotencyKey, "SUCCESS", { EX: 60 * 5 });
+    await redis.set(idempotencyKey, "SUCCESS", "EX", 60 * 5);
 
-    return res.status(200).json({ message: "Withdrawal Successful." });
+    return sendResponse(res, 200, "Withdrawal successful");
   } catch (error) {
-    await session.abortTransaction();
+    if (session.inTransaction()) {
+      await session.abortTransaction();
+    }
     if (transaction) {
       await Transactions.findByIdAndUpdate(transaction._id, {
         status: "failed",
       });
-      await redisClient.set(idempotencyKey, "FAILED", { EX: 60 * 5 });
+    }
+    if (idempotencyKey) {
+      await redis.set(idempotencyKey, "FAILED", "EX", 60 * 5);
     }
 
-    return res.status(500).json({ error: error.message });
+    next(error);
   } finally {
     await session.endSession();
   }
