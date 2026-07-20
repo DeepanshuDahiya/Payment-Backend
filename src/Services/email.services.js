0a860acf9207377ea8a02bf04a5f2148961dab07
DeepanshuDahiya <deepanshu31232@gmail.com>
2026-07-21 01:48:47 +0530

    remove / in import in email.services.js

--- src/Services/email.services.js
+++ src/Services/email.services.js
@@ -1,5 +1,5 @@
 import { otpTemplate } from "../Templates/otp.template.js";
-import transporter from "../Config/mail.js/";
+import transporter from "../Config/mail.js";
 
 export async function sendMail({ to, subject, html, text = "" }) {
   try {
